23f9c36a2fffb3a8266b3eed0129e760b8ae29ba
Marin Leontenko <marin.leontenko@gmail.com>
2018-02-28 13:31:37 +0100

    reducers + containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: TheGood Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
