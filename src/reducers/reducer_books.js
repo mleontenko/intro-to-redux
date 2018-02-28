aea32af944c1efd30dcbef6598317ac54d7edee9
Marin Leontenko <marin.leontenko@gmail.com>
2018-02-28 14:16:34 +0100

    implementation of a container class + bugfix

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: TheGood Parts' },
+        { title: 'Javascript: The Good Parts' },
         { title: 'Harry Potter' },
         { title: 'The Dark Tower' },
         { title: 'Eloquent Ruby' }
-    ]
+    ];
 }
